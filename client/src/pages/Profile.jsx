a4b83aeb372c2108dea3ff72b12f58ec46597066
Yaminee Shukla <yamineeshukla2000@gmail.com>
2024-06-14 12:09:08 +0530

    add create listing app

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -32,34 +32,35 @@ export default function Profile() {
       // request.resource.contentType.matches('images/.*')
 
 
-    useEffect(()=>{
-      if(file){
-         handleFileUpload(file);
-      }
-    },[file]);
+ useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
 
-    const handleFileUpload = (file) =>{
-      const storage = getStorage(app);
-      const fileName = new Date().getTime()+file.name;
-      const storageRef = ref(storage,fileName);
-      const uploadTask = uploadBytesResumable(storageRef,file);
-      
-      uploadTask.on('state_changed',
-      (snapshot)=>{
-        const progress = (snapshot.bytesTransferred/
-        snapshot.totalBytes)*100;
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         setFilePerc(Math.round(progress));
       },
-      (error)=>{
+      (error) => {
         setFileUploadError(true);
       },
-      ()=>{
-         getDownloadURL(uploadTask.snapshot.ref).then ((downloadURL)=>
-          setFormData({...FormData, avatar: downloadURL})
-         );
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
       }
-      );   
-    };
+    );
+  };
 
     const handleChange =(e)=>{
       setFormData({...formData, [e.target.id]:e.target.value});
@@ -130,31 +131,32 @@ export default function Profile() {
       <h1 className="text-3xl font-semibold text-center
        my-7">Profile</h1>
        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input onChange={(e)=>setFile(e.target.files[0])} 
-        type="file" ref={fileRef} hidden accept='image/*'/>
-        
-        <img 
-        onClick ={()=>fileRef.current.click()}src=
-        {formData.avatar||currentUser.avatar} alt="profile"
-          className='rounded-full h-24 w-24 object-cover
-          cursor-pointer self-center mt-2'
-          />
-       <p className='text-sm self-center'>
-         {fileUploadError?
-         (<span className='text-red-700'>Error Image upload(image must
-          be less than 2 mb)
-         </span>
-         ):filePerc>0 && filePerc<100 ? (
-          <span className='text-slate-700'>
-            {`Uploading ${filePerc}%`}
-          </span>
-          ):filePerc === 100? (
-            <span className='text-green-700'>Image successfully
-            uploaded!</span>
-          ): (
-          ''
+         <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type='file'
+          ref={fileRef}
+          hidden
+          accept='image/*'
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
+          alt='profile'
+          className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+        <p className='text-sm self-center'>
+          {fileUploadError ? (
+            <span className='text-red-700'>
+              Error Image upload (image must be less than 2 mb)
+            </span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className='text-green-700'>Image successfully uploaded!</span>
+          ) : (
+            ''
           )}
-         </p>
+        </p>
 
           <input 
           type="text" 
