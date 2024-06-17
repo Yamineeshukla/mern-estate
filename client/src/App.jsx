434c705c09735557f4ea55659015f714491cac46
Yaminee Shukla <yamineeshukla2000@gmail.com>
2024-06-17 12:19:37 +0530

    complete upload listing image funcitionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
 
 
 export default function App() {
@@ -18,7 +19,7 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn/>}/>
       <Route element={<PrivateRoute/>}>
       <Route path="/profile"element={<Profile/>}/>
-      <Route path='/create-listing' element={<Profile/>}/>
+      <Route path='/create-listing' element={<CreateListing/>}/>
       </Route>
     </Routes>
   </BrowserRouter>;
