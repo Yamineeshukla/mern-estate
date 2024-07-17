f90a94039024db57e6c5c90876718bbae6d94d0b
Yaminee Shukla <yamineeshukla2000@gmail.com>
2024-07-17 17:07:00 +0530

    create the listing item component

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -87,7 +87,8 @@ export default function Listing() {
               Link copied!
             </p>
           )}
-          <div className='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
+          <div className
+          ='flex flex-col max-w-4xl mx-auto p-3 my-7 gap-4'>
             <p className='text-2xl font-semibold'>
               {listing.name} - ${' '}
               {listing.offer
